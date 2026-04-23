934bb0688fbace6e616490845e738e2c7239f53e
Andrew C. Kim <andychokim@gmail.com>
2026-04-24 00:40:03 +0900

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        // no two pointers from left to right -> no determinator to shrink
+        // try two pointers from i and i+1
+        let buy = 0;
+        let sell = 1;
+        let res = 0;
+
+        while (sell < prices.length) {
+            if (prices[buy] < prices[sell]) {
+                let profit = prices[sell] - prices[buy];
+                res = Math.max(res, profit);
+                sell++;
+            }
+            else {
+                buy = sell;
+                sell = buy + 1;
+            }
+        }
+        return res;
+
+        // time: O(n), n: prices.length
+        // space: O(1), just two pointer variables
+    }
+}
