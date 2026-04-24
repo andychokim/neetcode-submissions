a88a86ba3d2265bd869a26d21625ecfe24c8400f
Andrew C. Kim <andychokim@gmail.com>
2026-04-25 01:48:00 +0900

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+    lowestCommonAncestor(root, p, q) {
+        // BST - so we can be more efficient with p and q search
+
+        // DFS pre-order approach (to pre-determine the direction to traverse)
+        // base cases
+            // if null node -> no LCA possible. empty return
+        // at each node - comparing root.val to p and q
+            // if root.val is in between (inclusive) of p and q
+                // found LCA -> return it
+            // if root.val is bigger than Math.max(p, q)
+                // go left
+            // if root.val is smaller than Math.min(p, q)
+                // go right
+        // then I pass - direction to traverse
+            // if go left -> go left
+            // if go right -> go right
+        
+        // base
+        if (!root) return;
+
+        // recursion
+        if (root.val > Math.max(p.val, q.val)) return this.lowestCommonAncestor(root.left, p, q);
+        if (root.val < Math.min(p.val, q.val)) return this.lowestCommonAncestor(root.right, p, q);
+        return root;
+
+        // **pre-order works easier since we CAN judge the direction to proceed with DFS
+        // WITH the information in current node. -> info first, action based on it
+        // **post-order would work better if we need the children nodes' information for
+        // finding the CURRENT node's answer. -> action first, info based on it
+        // ** "Do I already know the answer?" → base case, return immediately
+        // ** "Do I need to decide where to go before recursing?" → pre-order decision
+    }
+}
