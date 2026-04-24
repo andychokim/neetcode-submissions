3d7a68b360ba74ed7cad8151a059679bd5b1ee99
Andrew C. Kim <andychokim@gmail.com>
2026-04-24 16:33:43 +0900

    Add: depth-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-3.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+        // post-order DFS to find the longest path (depth) from the root node
+        // I hand off "What's the max depth of your subtree"
+            // to left n right children
+            // if a child is null, its an end so the max depth would be 0
+                // thus return 0
+        // Once both come back, I will
+            // return Math.max(left, right) + 1
+        
+        // base case:
+        if (!root) return 0;
+
+        // recursive case:
+        let leftMax = this.maxDepth(root.left);
+        let rightMax = this.maxDepth(root.right);
+
+        return Math.max(leftMax, rightMax) + 1;
+    }
+}
