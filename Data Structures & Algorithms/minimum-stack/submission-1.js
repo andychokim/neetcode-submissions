5d8e0833d89146a733227996388ac90aab64bed8
Andrew C. Kim <andychokim@gmail.com>
2026-04-26 02:27:46 +0900

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.js
@@ -0,0 +1,44 @@
+class MinStack {
+    constructor() {
+        this.stack = new Array();
+        this.min = new Array();
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        if (this.min.length === 0) this.min.push(val);
+        else {
+            // stack: 1 0 2
+            // min:   1 0 0
+            this.min.push(Math.min(val, this.min[this.min.length - 1]));
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.min.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.min[this.min.length - 1];
+    }
+
+    // **
+}
