417de2ad8ce4e09db3705f42aec7ec0b96d68125
Andrew C. Kim <andychokim@gmail.com>
2026-04-26 03:17:39 +0900

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        // for each char of tokens
+            // if char is not an operator,
+                // change it to number
+                // push the number into a temp array
+            // if char is an operator,
+                // run the calculation with 2-last numbers in the array
+                    // pop twice for each number -> prevents infinite expansion of the array(stack)
+                // push the result in the array
+
+        let stack = new Array();
+
+        for (let char of tokens) {
+            let num1;
+            let num2;
+            switch (char) {
+                case '+':
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    stack.push(num1 + num2);
+                    console.log('+', stack[stack.length - 1]);
+                    break;
+                case '-':
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    stack.push(num2 - num1); // watch out the popping order
+                    console.log('-', stack[stack.length - 1]);
+                    break;
+                case '*':
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    stack.push(num1 * num2);
+                    console.log('*', stack[stack.length - 1]);
+                    break;
+                case '/':
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    stack.push(Math.trunc(num2 / num1)); // truncates to zero, not to bottom
+                    console.log('/', stack[stack.length - 1]);
+                    break;
+                default:
+                    stack.push(Number(char));
+            }
+        }
+        return (stack.pop());
+    }
+}
