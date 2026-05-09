257e0d4d748eb68a7f686bb4ce015d2867998377
Andrew C. Kim <andychokim@gmail.com>
2026-05-09 23:02:14 +0900

    Add: design-hashset - submission-3

---
+++ Data Structures & Algorithms/design-hashset/submission-3.js
@@ -0,0 +1,41 @@
+class MyHashSet {
+    // static hashing (modulo operation)
+    // - constraints says max key value is 1000000 - we mod inputs by 1000000
+    constructor() {
+        this.val = 1000000
+        this.list = new Array(this.val + 1); // add 1 to have "indices" up to 1000000
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        this.list[key] = true;
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        this.list[key] = null;
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        if (!this.list[key]) return false;
+        return true;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
