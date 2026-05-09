131415fb2b21ae04eafe11a4670ff23af362d079
Andrew C. Kim <andychokim@gmail.com>
2026-05-09 19:24:38 +0900

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        // signs: remove in-place, order does not matter
+        // pattern: two pointers (swap), queue
+            // brute forcing - O(n^2)
+            // use queue(or stack) to make the process O(n)
+            // use two pointers to record forwarding i and recording j
+
+        // 1 0 1 0 4
+        let swapi = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === val) {
+                let temp = nums[swapi];
+                nums[swapi] = nums[i];
+                nums[i] = temp;
+                swapi++;
+            }
+        }
+        
+        for (let i = 0; i < swapi; i++) nums.shift();
+
+        return nums.length;
+    }
+}
